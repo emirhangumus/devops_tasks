713830587326eb9bfada3e2ad0c6fdb576612bff
emirhangumus <me@emirhangumus.com>
2023-08-11 18:58:21 +0000

    better accurate

--- task_1/script.sh
+++ task_1/script.sh
@@ -6,8 +6,11 @@ BBlue='\033[1;34m'
 NC='\033[0m'
 fname="$fname.txt"
 
-if [ -f "$fname" ]; then
+isExist=$(find . -maxdepth 1 -type f -exec basename {} \; | cut -c 18- | grep -E "^$fname" | wc -l)
+
+if [ $isExist -gt 0 ]; then
     echo "$fname dosyasının içeriği:"
+    fname=$(find . -maxdepth 1 -type f -exec basename {} \; | grep -E "*_$fname" | awk '{ print length, $0 }' | sort -n | head -n 1 | cut -d ' ' -f 2-)
     start=1;
     while IFS= read -r line; do
 	    echo -ne "${BBlue}${start}:${NC} $line"
@@ -16,13 +19,14 @@ if [ -f "$fname" ]; then
     done < "$fname"
 else
     echo "$fname dosyası bulunamadı. Oluşturuluyor..."
-    touch "$fname"
-
+    
     dateHour=$(date +"%Y-%m-%d_%H-%M")
+    
+    touch "${dateHour}_$fname"
 
     for i in {1..3}; do
         cName="${dateHour}_copy${i}_${fname}"
-        cp "$fname" "$cName"
+        cp "${dateHour}_$fname" "$cName"
         echo "$cName oluşturuldu"
     done
 fi
